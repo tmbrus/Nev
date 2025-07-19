10b5547928238a07c99966523b08163ca045b902
tmbrus <tmbrus.kurohtin@yandex.ru>
2025-07-19 11:23:11 +0300

    test

--- main.py
+++ main.py
@@ -1,57 +1,22 @@
-class BooksCollector:
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-    def __init__(self):
-        self.books_genre = {}
-        self.favorites = []
-        self.genre = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
-        self.genre_age_rating = ['Ужасы', 'Детективы']
+# 1. Укажите путь к yandexdriver.exe (скачайте его с GitHub)
+service = Service(r'C:\WebDriver\bin\yandexdriver.exe')  # Пример пути, замените на свой
 
-    # добавляем новую книгу
-    def add_new_book(self, name):
-        if not self.books_genre.get(name) and 0 < len(name) < 41:
-            self.books_genre[name] = ''
+# 2. Укажите путь к Яндекс.Браузеру (обычно в AppData)
+options = Options()
+options.binary_location = r'C:\Users\tmbru\AppData\Local\Yandex\YandexBrowser\Application\browser.exe'
 
-    # устанавливаем книге жанр
-    def set_book_genre(self, name, genre):
-        if name in self.books_genre and genre in self.genre:
-            self.books_genre[name] = genre
+# 3. Запуск браузера
+driver = webdriver.Chrome(service=service, options=options)
 
-    # получаем жанр книги по её имени
-    def get_book_genre(self, name):
-        return self.books_genre.get(name)
+# Открыть страницу
+driver.get("https://ya.ru")
 
-    # выводим список книг с определённым жанром
-    def get_books_with_specific_genre(self, genre):
-        books_with_specific_genre = []
-        if self.books_genre and genre in self.genre:
-            for name, book_genre in self.books_genre.items():
-                if book_genre == genre:
-                    books_with_specific_genre.append(name)
-        return books_with_specific_genre
+# Проверка (выведет версию браузера)
+print("Версия Яндекс.Браузера:", driver.capabilities['browserVersion'])
 
-    # получаем словарь books_genre
-    def get_books_genre(self):
-        return self.books_genre
-
-    # возвращаем книги, подходящие детям
-    def get_books_for_children(self):
-        books_for_children = []
-        for name, genre in self.books_genre.items():
-            if genre not in self.genre_age_rating and genre in self.genre:
-                books_for_children.append(name)
-        return books_for_children
-
-    # добавляем книгу в Избранное
-    def add_book_in_favorites(self, name):
-        if name in self.books_genre:
-            if name not in self.favorites:
-                self.favorites.append(name)
-
-    # удаляем книгу из Избранного
-    def delete_book_from_favorites(self, name):
-        if name in self.favorites:
-            self.favorites.remove(name)
-
-    # получаем список Избранных книг
-    def get_list_of_favorites_books(self):
-        return self.favorites
+# Закрыть браузер (раскомментируйте, когда будете готовы)
+# driver.quit()
